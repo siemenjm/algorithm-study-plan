9177b58e886f7aef8a8c126a6f74c1e65ad958bd
Jared Siemen <siemenjm@gmail.com>
2022-10-25 09:31:33 -0400

    Set up algo

---
+++ squares_of_a_sorted_array.js
@@ -0,0 +1,8 @@
+// Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+function sortedSquares() {
+
+}
+
+console.log(sortedSquares([-4,-1,0,3,10])); // [0,1,9,16,100]
+console.log(sortedSquares([-7,-3,2,3,11])); // [4,9,9,49,121]
