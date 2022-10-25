5dd9efec9bbd8f6585cd68e210f74a1bb7e78da9
Jared Siemen <siemenjm@gmail.com>
2022-10-25 09:27:25 -0400

    Solved

--- search_insert_position.js
+++ search_insert_position.js
@@ -3,7 +3,15 @@
 // You must write an algorithm with O(log n) runtime complexity.
 
 function searchInsertPosition(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] >= target) {
+            return i;
+        }
+    }
 
+    return nums.length;
 }
 
-console.log(searchInsertPosition([1,3,5,6], 5));
+console.log(searchInsertPosition([1,3,5,6], 5)); // 2
+console.log(searchInsertPosition([1,3,5,6], 2)); // 1
+console.log(searchInsertPosition([1,3,5,6], 7)); // 4
