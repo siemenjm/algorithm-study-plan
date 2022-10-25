7b94b123505beff02b10c54257b39e0ea47b08aa
Jared Siemen <siemenjm@gmail.com>
2022-10-25 09:40:31 -0400

    Add mergeSort function

--- squares_of_a_sorted_array.js
+++ squares_of_a_sorted_array.js
@@ -4,5 +4,17 @@ function sortedSquares() {
 
 }
 
+function mergeSort(unsortedArray) {
+    if (unsortedArray.length <= 1) {
+        return unsortedArray;
+    }
+
+    const middle = Math.floor(unsortedArray.length / 2);
+    const left = unsortedArray.slice(0, middle);
+    const right = unsortedArray.slice(middle);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
 console.log(sortedSquares([-4,-1,0,3,10])); // [0,1,9,16,100]
 console.log(sortedSquares([-7,-3,2,3,11])); // [4,9,9,49,121]
