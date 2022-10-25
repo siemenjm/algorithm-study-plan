5631010078e93eb31946d25eb099eeb3373ea4a2
Jared Siemen <siemenjm@gmail.com>
2022-10-25 09:19:11 -0400

    Set up algo

---
+++ search_insert_position.js
@@ -0,0 +1,9 @@
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+function searchInsertPosition(nums, target) {
+
+}
+
+console.log(searchInsertPosition([1,3,5,6], 5));
