715d677ba48c4d0debd8801e40f66af2de6f345e
Jared Siemen <siemenjm@gmail.com>
2022-10-25 09:58:36 -0400

    Solved, but time limit exceeded...

--- rotate_array.js
+++ rotate_array.js
@@ -1,8 +1,12 @@
 // Given an array, rotate the array to the right by k steps, where k is non-negative.
 
 function rotate(nums, k) {
+    for (let i = 1; i <= k; i++) {
+        nums.unshift(nums.pop());
+    }
 
+    return nums;
 }
 
-console.log([1,2,3,4,5,6,7], 3); // [5,6,7,1,2,3,4]
-console.log([-1,-100,3,99], 2); // [3,99,-1,-100]
+console.log(rotate([1,2,3,4,5,6,7], 3)); // [5,6,7,1,2,3,4]
+console.log(rotate([-1,-100,3,99], 2)); // [3,99,-1,-100]
