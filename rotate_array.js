4efa327713dccc0b7a704a3d6ffab469b8ac1290
Jared Siemen <siemenjm@gmail.com>
2022-10-25 10:09:48 -0400

    Solved, but not in place...

--- rotate_array.js
+++ rotate_array.js
@@ -1,11 +1,16 @@
 // Given an array, rotate the array to the right by k steps, where k is non-negative.
 
 function rotate(nums, k) {
-    for (let i = 1; i <= k; i++) {
-        nums.unshift(nums.pop());
-    }
+    // for (let i = 1; i <= k; i++) {
+    //     nums.unshift(nums.pop());
+    // }
 
-    return nums;
+    // return nums;
+
+    const first = nums.slice(-k);
+    const resultArray = first.concat(nums.slice(0, -k));
+
+    return resultArray;
 }
 
 console.log(rotate([1,2,3,4,5,6,7], 3)); // [5,6,7,1,2,3,4]
