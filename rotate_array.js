60e2cd75b8d3ca9d3327af6d1bc1f83ac98b2b63
Jared Siemen <siemenjm@gmail.com>
2022-10-25 09:51:39 -0400

    Set up algo

---
+++ rotate_array.js
@@ -0,0 +1,8 @@
+// Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+function rotate(nums, k) {
+
+}
+
+console.log([1,2,3,4,5,6,7], 3); // [5,6,7,1,2,3,4]
+console.log([-1,-100,3,99], 2); // [3,99,-1,-100]
