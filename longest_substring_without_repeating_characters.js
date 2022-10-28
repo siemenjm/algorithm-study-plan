b366cf7270b20e27cc18a03421e3f9398964ad45
Jared Siemen <siemenjm@gmail.com>
2022-10-28 11:43:24 -0400

    Solved with help

--- longest_substring_without_repeating_characters.js
+++ longest_substring_without_repeating_characters.js
@@ -20,40 +20,73 @@
 // 0 <= s.length <= 5 * 104
 // s consists of English letters, digits, symbols and spaces.
 
-function lengthOfLongestSubstring(s) {
-    // find all substings
-    // ignore substrings that have repeated characters?...
-    // return remaining substring with longest length
+// function lengthOfLongestSubstring(s) {
+//     // find all substings
+//     // ignore substrings that have repeated characters?...
+//     // return remaining substring with longest length
 
-    let substringList = [];
-    for (let i = 0; i < s.length; i++) {
-        let currentSubstring = [];
-        for (let j = i; j < s.length; j++) {
-            if (!currentSubstring.includes(s[j])) {
-                currentSubstring.push(s[j]);
-                const thisSubstring = currentSubstring.join('');
+//     let substringList = [];
+//     for (let i = 0; i < s.length; i++) {
+//         let currentSubstring = [];
+//         for (let j = i; j < s.length; j++) {
+//             if (!currentSubstring.includes(s[j])) {
+//                 currentSubstring.push(s[j]);
+//                 const thisSubstring = currentSubstring.join('');
                 
-                if (!substringList.includes(thisSubstring)) {
-                    substringList.push(thisSubstring);
-                }
-            } else {
-                break;
+//                 if (!substringList.includes(thisSubstring)) {
+//                     substringList.push(thisSubstring);
+//                 }
+//             } else {
+//                 break;
+//             }
+//         }
+//     }
+
+//     let longestSubstring = {
+//         string: '',
+//         length: 0
+//     };
+//     for (let i = 0; i < substringList.length; i++) {
+//         if (substringList[i].length > longestSubstring.length) {
+//             longestSubstring.string = substringList[i];
+//             longestSubstring.length = substringList[i].length;
+//         }
+//     }
+
+//     return longestSubstring.length;
+// }
+
+function lengthOfLongestSubstring(s) {
+    // track longest string and current string
+    // check if current letter is in current string
+    // if above is true -> compare current string length to longest string length, replace if longer
+    // if about is false -> add letter to current string
+    // return longest string length
+
+    let longestString = '';
+    let currentString = '';
+
+    for (let i = 0; i < s.length; i++) {
+        let currentLetter = s[i];
+        let index = currentString.indexOf(currentLetter);
+
+        if (index > -1) {
+            if (currentString.length > longestString.length) {
+                longestString = currentString;
             }
+
+            currentString = currentString.slice(index + 1) + currentLetter;
+        } else {
+            currentString += currentLetter;
         }
     }
 
-    let longestSubstring = {
-        string: '',
-        length: 0
-    };
-    for (let i = 0; i < substringList.length; i++) {
-        if (substringList[i].length > longestSubstring.length) {
-            longestSubstring.string = substringList[i];
-            longestSubstring.length = substringList[i].length;
-        }
+    if (currentString.length > longestString.length) {
+        longestString = currentString;
     }
 
-    return longestSubstring.length;
+    return longestString.length;
+
 }
 
 console.log(lengthOfLongestSubstring('tree')); // tre?...
